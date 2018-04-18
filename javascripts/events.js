50e57f02113260ce452c385c1fadefe14394f3e1
Nathan Pabst <nathan.pabst@gmail.com>
2018-04-18 17:21:38 -0500

    completed evenetListeners card

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,16 +1,43 @@
 const catButton = document.getElementById('cats');
-// const dogButton = document.getElementById('dog');
-// const dinoButton = document.getElementById('dino');
+const dogButton = document.getElementById('dogs');
+const dinoButton = document.getElementById('dinos');
 
 const showCats = (e) => {
-  console.log(e.target.children[0].classList.add('hidden'));
-  // e.target.children[].classList.add('hidden');
+  hideAnimal('dog');
+  hideAnimal('dino');
+  showAnimal('cat');
+};
+
+const showDogs = (e) => {
+  hideAnimal('cat');
+  hideAnimal('dino');
+  showAnimal('dog');
+};
+
+const showDinos = (e) => {
+  hideAnimal('cat');
+  hideAnimal('dog');
+  showAnimal('dino');
+};
+
+const hideAnimal = (type) => {
+  const collection = document.getElementsByClassName(type);
+  for (let i = 0; i < collection.length; i++) {
+    collection[i].classList.add('hidden');
+  }
+};
+
+const showAnimal = (type) => {
+  const collection = document.getElementsByClassName(type);
+  for (let i = 0; i < collection.length; i++) {
+    collection[i].classList.remove('hidden');
+  }
 };
 
 const allAnimalButtons = () => {
   catButton.addEventListener('click', showCats);
-  // dogButton.addEventListener('click', showDogs);
-  // dinoButton.addEventListener('click', showDinos);
+  dogButton.addEventListener('click', showDogs);
+  dinoButton.addEventListener('click', showDinos);
 };
 
 module.exports = {
